ae9c788e4fd29384f8550eae1a62555952366f03
Carnot <narayane.rm@gmail.com>
2022-09-21 10:29:37 -0300

    add Navbar Icon

--- react-website/src/App.js
+++ react-website/src/App.js
@@ -1,10 +1,19 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/' exact/>
+      </Routes>
+    </Router>
+    </>
+      
   );
 }
 
--- react-website/src/components/Navbar.js
+++ react-website/src/components/Navbar.js
@@ -1,12 +1,13 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 
 function Navbar() {
   return (
     <>
-    <nav className="navbar">
-        <div className="navbar-container">
-         <Link to="/" className="navbar-logo">
-            TRVL
+    <nav className='navbar'>
+        <div className='navbar-container'>
+         <Link to='/' className='navbar-logo'>
+            TRVL <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
         </Link>    
         </div>
     </nav>
