b31a76ab35cbbfd01b2569a6c8472123dbc790ff
Carnot <narayane.rm@gmail.com>
2022-09-20 21:58:44 -0300

    Update Navbar

---
+++ react-website/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+
+function Navbar() {
+  return (
+    <>
+    <nav className="navbar">
+        <div className="navbar-container">
+         <Link to="/" className="navbar-logo">
+            TRVL
+        </Link>    
+        </div>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
